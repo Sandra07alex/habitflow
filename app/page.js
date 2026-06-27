6f363023d75857ac1194d2cd76f2abcbdfe5da67
sandra07alex <sandra5617alex@gmail.com>
2026-06-27 18:53:02 +0530

    herosection compledted

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
+import Hero from "@/components/Landing/Hero/Hero";
+import Journey from "@/components/Landing/Journey/Journey";
+
 export default function Home() {
-  return null;
+  return (
+    <main>
+      <Hero />
+      <Journey />
+    </main>
+  );
 }
